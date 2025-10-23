cf38e64406bc487ee837136ca70e98ed0248fe1b
GlassesMan-031 <karelindqvist@gmail.com>
2025-10-23 11:56:59 +0200

    Fix starting point, add Chakra UI, add basic Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import { useEffect } from 'react'
 
+import { Heading, Highlight, Text} from '@chakra-ui/react'
 
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
-    useEffect(() => {
-    fetch('/api')
-      .then((response) => response.json())
-      .then((result) => {
-        alert(`Hello ${result.hello}!`)
-      })
-  }, [])
 
 
   return (
-    <>
+  <>
 
-    </>
+    <Heading size="3xl" letterspacing="tight">
+      <Highlight query="populating the page" styles={{color:"teal.600"}}>
+      Let's begin populating the page with content
+     </Highlight>
+      </Heading>
+  <Highlight query="Two Boxes" styles={{ fontWeight: "semibold", color:"teal.600" }}>
+      We're gonna generate Two Boxes and inject our Database content into them
+    </Highlight>
+
+</>
   )
 }
 
