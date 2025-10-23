57d616d01ed93a5a08efeec12aa4ed0bbd5959f0
GlassesMan-031 <karelindqvist@gmail.com>
2025-10-23 13:09:03 +0200

    CSS changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 
-import { Heading, Highlight, Text} from '@chakra-ui/react'
-
+import { Heading, Highlight, Center, Box} from '@chakra-ui/react'
 
 
 
@@ -10,15 +9,20 @@ function App() {
 
   return (
   <>
-
+    <Box display="flex" alignItems="center" justifyContent="center">
     <Heading size="3xl" letterspacing="tight">
       <Highlight query="populating the page" styles={{color:"teal.600"}}>
       Let's begin populating the page with content
      </Highlight>
       </Heading>
+      </Box>
+
+<Box display="flex" alignItems="center" justifyContent="center">
   <Highlight query="Two Boxes" styles={{ fontWeight: "semibold", color:"teal.600" }}>
       We're gonna generate Two Boxes and inject our Database content into them
     </Highlight>
+          </Box>
+
 
 </>
   )
